3b1092f8274ae79ecfa2ca14223f2ccf752fd895
vishal sarode <vsarode@umassd.edu>
2016-02-24 19:43:23 -0500

    JUnit test case for the java class Date.java Starting with test to check valid function working.....

---
+++ src/lab2/DateTest.java
@@ -0,0 +1,23 @@
+package lab2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DateTest {
+
+	@Test
+	public void testIsLegitimate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdvanceDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddDay() {
+		fail("Not yet implemented");
+	}
+}
